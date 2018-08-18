6fd4dca08861f8f869a121fccd042e7100d0c106
Vince Broz <vince@broz.cc>
2018-08-18 14:56:14 -0400

    Ratchet type coverage

--- types/stubs/pysectools/pinentry/__init__.pyi
+++ types/stubs/pysectools/pinentry/__init__.pyi
@@ -1,2 +1,19 @@
+from typing import Optional, Callable
+
+
 class Pinentry:
-    pass
+    def __init__(self,
+                 pinentry_path: str="pinentry",
+                 fallback_to_getpass: bool=True) -> None:
+        ...
+
+    def ask(self,
+            prompt: str="Enter the password: ",
+            description: Optional[str]=None,
+            error: str="Wrong password!",
+            validator: Callable[[Optional[object]], bool]=
+            lambda x: x is not None) -> str:
+        ...
+
+    def close(self) -> None:
+        ...
